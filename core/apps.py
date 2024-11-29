68c5d8baf78732e438e3cae764e2fde48f16a5da
antoniooracio <antoniooracio@gmail.com>
2024-11-29 11:20:38 -0300

    Delete core directory

--- core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-    verbose_name = 'Menu Backup'  # Nome que será exibido no Django Admin
